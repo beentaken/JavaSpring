2a0105c6a772570460d1c9fc7b99ec0d7fe249ed
Sohel Sorowar <sohelsorowar@gmail.com>
2017-07-17 18:17:49 +0600

    RUAPI

---
+++ RU_RESTful_API/src/test/java/com/RU_RESTful_API/mysql/ApplicationTests.java
@@ -0,0 +1,18 @@
+package com.RU_RESTful_API.mysql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	
+	
+	@Test
+	public void contextLoads() {
+	}
+
+}
